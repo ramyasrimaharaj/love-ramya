c36445d7c7c460bc620c5c1d471fe0e405dc46c5
kalyangoud77 <kalyangoud2010@gmail.com>
2019-06-19 11:56:02 +0530

    I changed love symbol...bcz i hate love

--- love.py
+++ love.py
@@ -1,7 +1,9 @@
+#i helped you in this project
+
 for row in range(0,6):
 	for col in range(0,7):
 		if((row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8)):
-			print('@',end='')
+			print('*',end='')
 		else:
 			print(" ",end='')
 	print()
